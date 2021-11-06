2e7f0d8a0ec94fcf89f412888e336e81656c0827
hezy laplacian <hezylaplacian@gmail.com>
2021-11-06 19:23:31 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import { NewsletterService } from './services/newsletter.service';
+import { UserService } from './services/user.service';
+import { ChangeDetectorRefComponent } from './change-detector-ref/change-detector-ref.component';
+import { CdChildComponent } from './change-detector-ref/cd-child/cd-child.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, NewsletterComponent, ChangeDetectorRefComponent, CdChildComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [UserService, NewsletterService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
